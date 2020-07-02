b5e2855fe2b3d514f83f23ed0cee1f5a7ae6166a
rebecca136 <54754930+rebecca136@users.noreply.github.com>
2020-07-02 16:19:39 -0400

    I remove the variable constant and use scanner to input my variable

--- teamA/src/main/java/com/aipalbot/teamA/App.java
+++ teamA/src/main/java/com/aipalbot/teamA/App.java
@@ -1,5 +1,7 @@
 package com.aipalbot.teamA;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,56 +10,79 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       // System.out.println( "Hello World!" );
+    	
+    	int row ;
+    	int col ;
+    	int row2 ;
+    	int  col2;
         
-        int a= 2;
-    	int a1 =2;
-    	int b =2;
-    	int b1 = 2;
-    
-    	
-    	
-    	int[][] matrixA= {{5,8},{7, 2}};
-    	int[][]  matrixB ={{3,1},{2,4}};
-    	
-    	//Multiplying
-    	
-    	int[][] arr = multiplyMatrix(matrixA, matrixB, a, b, b1);
-    		
-    	displayArr(arr);
-    	
+      Scanner input = new Scanner(System.in);
+		System.out.println("Enter the first no of row:-");
+		 row = input.nextInt();
+		
+		System.out.println("Enter the first no of col:-");
+		 col = input.nextInt();
 		
 		
-	}
-
-	public static int [][] multiplyMatrix(int[][] matrixA,int[][] matrixB,int a, int b, int b1 ){
-		
-		int[][] arr =new int[a][b1];
-		for(int i = 0; i< a; i++) {
-			for(int j = 0; j < b; j++) {
-				for(int k = 0; k < b1; k++) {
-					
-					arr[i][j] += matrixA[i][k] * matrixB[k][j];
-				}
+		System.out.println("print the result out:-");
+    	 row2 = input.nextInt();
+    	
+    	 System.out.println("print the result out:-");
+    	 col2 = input.nextInt();
+    	 
+    	 if(row==col)
+    	 {
+    	int matrixA[][]= new  int[row][col];
+    	int matrixB[][] = new int[row2][col2];
+    	int result[][]  = new int[row][col2];
+    	
+    System.out.println("Enter the element of matrix:-");
+   
+    	
+    	for(int i = 0; i< row; i++) 
+    	{
+			for(int j = 0; j < col; j++) 
+			
+				matrixA[i][j] = input.nextInt();
 				
 			}
-		}
-		
-		return arr;
+			
+			System.out.println("Enter the element of matrix2:-");
+			
+			for(int i = 0; i< row2; i++) 
+	    	{
+				for(int j = 0; j < col2; j++) 
+				
+					matrixB[i][j] = input.nextInt();
+			}
+			
+			 System.out.println("\n\noutput matrix:-");
+			
+			for(int i = 0; i< row; i++) 
+	    	
+			for(int j = 0; j < col2; j++) 
+				{
+				int sum = 0;
+					
+			for(int k = 0; k < row2; k++) {
+					
+				sum += matrixA[i][k] * matrixB[k][j];
+				}
+				
+		result[i][j] =sum;
+				
 	}
-	
-	public static void displayArr(int[][] arr) {
-		System.out.println(" The result of Two matric is :-");
-		
-		
-		 for(int[] row : arr) {
-			 
-			 for(int colum : row) {
-			 
-			 System.out.print(colum + "    ");
-		 }
-			 System.out.println();
-		
-		 }
+			for(int i = 0; i< row; i++) {
+		    	
+				for(int j = 0; j < col2; j++) 
+					
+					 System.out.print(result[i][j] + " ");
+					
+				System.out.println();
+	}
     }
+    	 else 
+    		 System.out.print("multiplication does not exist in the matrix");
+}
 }
